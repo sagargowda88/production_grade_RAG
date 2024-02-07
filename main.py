c32170ec3f80c033fd791fea4b965d6aedc321a8
Sagar Gowda <46260031+sagargowda88@users.noreply.github.com>
2024-02-07 15:39:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import logging
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import time
 from concurrent.futures import ThreadPoolExecutor
-from app import user_question, context
+from app import user_question, context  # Added import statement
 
 tokenizer = AutoTokenizer.from_pretrained("./SQL")
 model = AutoModelForCausalLM.from_pretrained("./SQL")
+logger = logging.getLogger(__name__)
 
 def get_sql_query(user_question, context):
     text = f"""CREATE TABLE stadium (
@@ -26,12 +28,12 @@ answer: SELECT"""
     generated_ids = model.generate(input_ids, max_length=2000)
     generated_text = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
     Answer = generated_text.split('\n')[-1]
-    print("Final ANswer: ", Answer)
-    print("Time taken by model: ", time.time()-start_time)
+    logger.info("Final answer: %s", Answer)
+    logger.info("Time taken by model: %s", time.time()-start_time)
     return Answer
 
 start_time2 = time.time()
 with ThreadPoolExecutor() as executor:
     future = executor.submit(get_sql_query, user_question, context)
-    print(future.result())
-print("Total Time Taken: ", time.time()-start_time2)
+    logger.info(future.result())
+logger.info("Total Time Taken: %s", time.time()-start_time2)
