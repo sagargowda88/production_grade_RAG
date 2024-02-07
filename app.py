02f40f1d12e8662d01736b118ec3355e28f0e4bc
Sagar Gowda <46260031+sagargowda88@users.noreply.github.com>
2024-02-07 15:38:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
-# streamlit_app.py
+import logging
 from vector_store import VectorStore
 import pandas as pd
 
-uploaded_file = './Training_data.csv'
+logger = logging.getLogger(__name__)
 
-data = pd.read_csv(uploaded_file, encoding_errors="ignore")
+def setup_application():
+    uploaded_file = './Training_data.csv'
+    data = pd.read_csv(uploaded_file, encoding_errors="ignore")
+    cols_to_index = data['instruction']
+    
+    vector_store = VectorStore()
+    vector_store.index_data(data, cols_to_index)
+    logger.info("Data indexed successfully.")
+    return vector_store
 
-# Allow the user to select a specific column for indexing
-cols_to_index = data['instruction']
-
-vector_store = VectorStore()
-vector_store.index_data(data, cols_to_index)
-print("Data indexed successfully!")
-
-user_question = "fetch me rule details where exception concept is book"
-query = user_question
-context = """
-"""
-if query:
+def get_similar_vectors(user_query, vector_store):
     try:
-        # result_ids = [uid for uid, _ in vector_store.find_similar_vectors(query, 5)]
-        # result_df = data.loc[result_ids].reset_index(drop=True)
-        # st.write(result_df)
-        result_ids_and_scores = vector_store.find_similar_vectors(query, 5)
-        result_df = data.loc[[uid for uid, _ in result_ids_and_scores]].reset_index(drop=True)
-        # print(result_df)
-        for index,row in result_df.iterrows():
-            context += f"question: {row['instruction']}\nanswer: {row['output']}\n\n"
-        # result_df["Similarity Score"] = [score for _, score in result_ids_and_scores]
-        
+        result_ids_and_scores = vector_store.find_similar_vectors(user_query, 5)
+        return result_ids_and_scores
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logger.error("An error occurred: %s", e)
+        return []
 
-print(context)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)  # Set up basic logging configuration
+    vector_store = setup_application()
+    user_question = "fetch me rule details where exception concept is book"
+    context = """
+"""
+    result_ids_and_scores = get_similar_vectors(user_question, vector_store)
+    for uid, score in result_ids_and_scores:
+        context += f"question: {data.loc[uid]['instruction']}\nanswer: {data.loc[uid]['output']}\n\n"
+        logger.info(f"Vector ID: {uid}, Similarity Score: {score}")
+    logger.info("Context: %s", context)
