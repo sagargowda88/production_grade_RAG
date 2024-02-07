e2e438dc02de1dee69dddacf718e43e1ac6faa4a
Sagar Gowda <46260031+sagargowda88@users.noreply.github.com>
2024-02-07 20:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+import sys
 import logging
 from vector_store import VectorStore
 import pandas as pd
@@ -6,9 +7,9 @@ import pandas as pd
 # Configure logging
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def upload_and_index_data(uploaded_file):
+def _upload_and_index_data(uploaded_file):
     """
-    This function is meant to be used only by developers to upload and index data.
+    This function is meant to be used only by the developer to upload and index data.
     """
     try:
         data = pd.read_csv(uploaded_file, encoding_errors="ignore")
@@ -28,29 +29,23 @@ def main():
     query = user_question
     context = ""
     try:
-        if len(sys.argv) > 1 and sys.argv[1] == "--upload-index":
-            # Developer mode: upload and index data
-            if not upload_and_index_data('./Training_data.csv'):
-                return
-        else:
-            # Regular mode: handle user queries
-            if query:
-                result_ids_and_scores = vector_store.find_similar_vectors(query, 5)
-                result_df = data.loc[[uid for uid, _ in result_ids_and_scores]].reset_index(drop=True)
-                for index, row in result_df.iterrows():
-                    context += f"question: {row['instruction']}\nanswer: {row['output']}\n\n"
-                logging.info("Context generated successfully.")
-                logging.info(context)
+        # Regular mode: handle user queries
+        if query:
+            data = pd.read_csv('./Training_data.csv', encoding_errors="ignore")
+            result_ids_and_scores = vector_store.find_similar_vectors(query, 5)
+            result_df = data.loc[[uid for uid, _ in result_ids_and_scores]].reset_index(drop=True)
+            for index, row in result_df.iterrows():
+                context += f"question: {row['instruction']}\nanswer: {row['output']}\n\n"
+            logging.info("Context generated successfully.")
+            logging.info(context)
         return user_question, context
 
     except Exception as e:
         logging.error(f"An error occurred: {e}", exc_info=True)
 
+# Call the _upload_and_index_data function only in developer mode
+if len(sys.argv) > 1 and sys.argv[1] == "--upload-index":
+    _upload_and_index_data('./Training_data.csv')
+
 if __name__ == "__main__":
     main()
-
-
-
-# python app.py --upload-index
-# This will execute the upload_and_index_data function and index the data. After the data is indexed, the script will exit, as it's in developer mode. 
-# When the script is run without the --upload-index argument, it will operate in regular mode and handle user queries.
